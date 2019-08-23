c46ae16dfad7e678c07679a0e484e0f7b6959076
mingzhi.xie <xie6032mail@163.com>
2019-08-23 15:53:10 +0800

    Add http interceptor

---
+++ spring-3.0.x-learning/src/main/java/com/miven/spring3/web/DefaultClientHttpRequestInterceptor.java
@@ -0,0 +1,30 @@
+package com.miven.spring3.web;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+/**
+ *  默认的http请求拦截器
+ * @author mingzhi.xie
+ * @date 2019/8/22
+ * @since 1.0
+ */
+public class DefaultClientHttpRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    private HttpHeaders headers;
+
+    public boolean containsKey(Object key) {
+        return headers.containsKey(key);
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        headers = request.getHeaders();
+        return execution.execute(request, body);
+    }
+}
