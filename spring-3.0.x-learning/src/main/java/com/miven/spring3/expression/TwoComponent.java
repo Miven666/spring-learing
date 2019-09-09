8ace67ef94c61664cf364bb01f74b09a2d1269de
mingzhi.xie <xie6032mail@163.com>
2019-09-09 17:38:46 +0800

    Add demo xmzComponent class

---
+++ spring-3.0.x-learning/src/main/java/com/miven/spring3/expression/TwoComponent.java
@@ -0,0 +1,12 @@
+package com.miven.spring3.expression;
+
+import com.miven.spring.context.XmzComponent;
+
+/**
+ * @author mingzhi.xie
+ * @date 2019/9/4
+ * @since 1.0
+ */
+@XmzComponent(2)
+public class TwoComponent {
+}
